f9a46785f5d3b291421a844347b883dd35fe4452
Guz9ka <rodder2017@yandex.ru>
2022-07-13 17:26:40 +0300

    Add contact create logics

--- ContactBook/AddContactPage.xaml.cs
+++ ContactBook/AddContactPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using ContactBook.Contact.Domain;
+using ContactBook.Contracts.Contact;
 
 namespace ContactBook
 {
@@ -11,6 +13,30 @@ namespace ContactBook
         {
             InitializeComponent();
             _contactService = contactService;
+
+            SaveContactButton.Click += SaveContactButtonOnClick;
+        }
+
+        private void SaveContactButtonOnClick(object s, RoutedEventArgs e)
+        {
+            var contactCreateModel = new ContactCreateModel
+            {
+                Name = Name.Text,
+                Surname = Surname.Text,
+                CompanyName = CompanyName.Text,
+                PhoneNumber = PhoneNumber.Text
+            };
+
+            _contactService.AddContact(contactCreateModel);
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            Name.Text = "";
+            Surname.Text = "";
+            CompanyName.Text = "";
+            PhoneNumber.Text = "";
         }
     }
 }
